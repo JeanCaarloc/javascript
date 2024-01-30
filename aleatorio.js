bbcceb1117968f3f592ff0db9d674af4d3548de3
jeancarlo_fcl@hotmail.com <a95287766@rn.senac.local>
2024-01-30 08:44:20 -0300

    atividade arrays

---
+++ aleatorio.js
@@ -0,0 +1,11 @@
+var palavrasEDicas = [
+    ['c','a','m','i','s','e','t','a','Vestimenta']
+    ['a','v','e','n','i','d','a','Rua grande']
+];
+var i=0
+var j=0
+    for(var i=0; i<palavrasEDicas.length; i++){
+        for(var j=0; j<palavrasEDicas[i].length; j++){
+        console.log(i+" "+j);
+        }
+    }    
---
+++ rodaRoda.js
@@ -0,0 +1,13 @@
+var palavraMisteriosa= [j,a,v,a,s,c,r,i,p,t]; palavraDescoberta=[];
+var tamanho = palavraMisteriosa.lenght;
+var inputUsuario;
+var contador=0;
+    while (palavraDescoberta!=palavraMisteriosa){
+        var inputUsuario;
+    }
+        for (contador=0;contador<tamanho; ) {
+        }    
+            if (palavraMisteriosa[contador] == inputUsuario){
+            palavraDescoberta[contador]=inputUsuario                      
+            }    
+                
