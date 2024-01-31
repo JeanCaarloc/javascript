3d1cb43464341546d602fb99d804647e66d02d70
jeancarlo_fcl@hotmail.com <a95287766@rn.senac.local>
2024-01-31 08:26:04 -0300

    matriz

---
+++ prog018.js
@@ -0,0 +1,16 @@
+var numeros = [
+    [1,2],
+    [3,4]
+];
+
+numeros.push([5, 6]); //insere elemento na última posição
+numeros.unshift([0,0]); //insere elemento na primeira posição
+numeros.pop(); //remove elemento da última posição
+numeros.shift(); //remove elemento da primeira posição
+
+console.log(numeros);//mostra a matriz
+
+
+
+
+
